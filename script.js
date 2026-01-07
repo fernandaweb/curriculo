4a0ffd5e76f9a6ba43dc168c5aeba3dc76527455
Fernanda Gontijo <148666628+fernandaweb@users.noreply.github.com>
2026-01-07 20:06:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ function mudouTamanho() {
     
 
 }
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        // Remove o estilo 'display: block/none' adicionado pelo JS
+        document.querySelector('.menu').style.display = ''; 
+    }
+});
+
+
+
+window.addEventListener('resize', mudouTamanho);
+window.addEventListener('load', mudouTamanho);
+window.addEventListener('scroll', mudouTamanho);
+window.addEventListener('click', mudouTamanho);
+window.addEventListener('mousemove', mudouTamanho);
+window.addEventListener('keydown', mudouTamanho);
+window.addEventListener('touchstart', mudouTamanho);
+window.addEventListener('touchmove', mudouTamanho);
+window.addEventListener('touchend', mudouTamanho);
+window.addEventListener('focus', mudouTamanho);
+window.addEventListener('blur', mudouTamanho);
+window.addEventListener('orientationchange', mudouTamanho);
+window.addEventListener('visibilitychange', mudouTamanho);
+window.addEventListener('beforeunload', mudouTamanho);
+window.addEventListener('unload', mudouTamanho);
+window.addEventListener('hashchange', mudouTamanho);
+
